8ac07aba894671e4d59c9de64d4cb6ad3f8f2349
Rafael Goto <rafael916goto@hotmail.com>
2018-04-14 02:14:20 -0300

    Chart offline set on fragmentw

--- app/src/main/java/br/senac/sp/aplicativopiv2/ChartLineFragment.kt
+++ app/src/main/java/br/senac/sp/aplicativopiv2/ChartLineFragment.kt
@@ -17,8 +17,25 @@ class ChartLineFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_chart_line, container, false)
+
+        val listData = ArrayList<Entry>()
+        listData.add(Entry(0f, 10F))
+        listData.add(Entry(1f, 20F))
+        listData.add(Entry(2f, 30F))
+        listData.add(Entry(3f, 100F))
+        listData.add(Entry(4f, 40F))
+        listData.add(Entry(5f, 200F))
+
+        val lineDataSet = LineDataSet(listData,"")
+        lineDataSet.color = ContextCompat.getColor(context, R.color.colorAccent)
+        lineDataSet.valueTextColor = ContextCompat.getColor(context, android.R.color.white)
+
+        val lineData = LineData(lineDataSet)
+        val lineChart = view.findViewById<View>(R.id.lineChart) as LineChart
+
+        lineChart.data = lineData
+        lineChart.invalidate() //refresh graph
 
 
         return view
